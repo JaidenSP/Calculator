2578278cd38bb85d5dd8cea63444a2eaecc24fe9
JaidenSP <jaidenpaterson3@gmail.com>
2022-06-27 12:21:30 +0930

    Added the operator functions and the operate function

---
+++ assets/app.js
@@ -0,0 +1,35 @@
+//Create basic + - / * functions and a main calculator function
+
+function additon(a, b){
+    let calculated = a + b;
+    return calculated;
+}
+
+function subtraction(a, b){
+    let calculated = a - b;
+    return calculated;
+}
+
+function division(a, b) {
+    let calculated = a / b;
+    return calculated; 
+}
+
+function multiplication(a, b){
+    let calculated = a * b;
+    return calculated;
+}
+
+//create an operate function that uses the other functions and calls on the function and numbers
+//create a basic calculator with html and css with an equals button and a clear button (create a display with for the buttons)
+//create the functions that populate the display (should be storing the display value in a variable outside of the scope for use later)
+
+function operate(additon, subtraction, division, multiplication){
+
+   additon = additon(16, 12);
+
+   console.log(additon);
+
+}
+
+operate();
