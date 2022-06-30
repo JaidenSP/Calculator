611725a8c2a2109bab1d900f3d539c6430304e95
JaidenSP <jaidenpaterson3@gmail.com>
2022-06-30 10:32:37 +0930

    Added event listeners to all button elements and used a function to display button clicked

--- assets/app.js
+++ assets/app.js
@@ -1,35 +1,54 @@
+//using the dom to maniuplate the html 
+const calculatorText = document.getElementById("display-text");
+
+document.querySelectorAll('button').forEach(el => {
+    el.addEventListener('click', () => {
+     displayInput(el.textContent);
+    });
+});
+
+calculatorText.textContent = "123456"
+
+
+//Store the button clicked value
+let displayValue = '';
+
+
 //Create basic + - / * functions and a main calculator function
 
-function addition(a, b, operate){
-    let calculated = a + b;
-    return operate(calculated);
-    
+function addition(a, b){
+   return a + b;
+        
 }
 
 function subtraction(a, b){
-    let calculated = a - b;
-    return calculated;
-}
+   return a - b;
+    }
 
 function division(a, b) {
-    let calculated = a / b;
-    return calculated; 
+   return a / b;
+    
 }
 
 function multiplication(a, b){
-    let calculated = a * b;
-    return calculated;
-}
+   return a * b;
+    }
 
 //create an operate function that uses the other functions and calls on the function and numbers
 //create a basic calculator with html and css with an equals button and a clear button (create a display with for the buttons)
-//create the functions that populate the display (should be storing the display value in a variable outside of the scope for use later)
 
-function operate(additon){
+function operate(func){
+    func();
 
-    var calculateAddition = additon;
-
-    return calculateAddition;
 }
 
-console.log(addition(12, 13, operate));
+//create the functions that populate the display (should be storing the display value in a variable outside of the scope for use later)
+
+function displayInput(digit){
+  
+  calculatorText.textContent = digit;
+
+}
+
+
+
