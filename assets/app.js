1802aeded396a94077d86c74a1a8f580ae7ed4c2
JaidenSP <jaidenpaterson3@gmail.com>
2022-07-01 10:41:59 +0930

    Operate function uses the previous operator functions and two numbers to calculate an equation

--- assets/app.js
+++ assets/app.js
@@ -1,7 +1,7 @@
 //using the dom to maniuplate the html 
 const calculatorText = document.getElementById("display-text");
 
-document.querySelectorAll('button').forEach(el => {
+let buttonsClicked = document.querySelectorAll('button').forEach(el => {
     el.addEventListener('click', () => {
      displayInput(el.textContent);
     });
@@ -11,7 +11,9 @@ calculatorText.textContent = "123456"
 
 
 //Store the button clicked value
-let displayValue = '';
+let firstNumberClicked = '';
+let operatorClicked = '';
+let finalOperatorClicked = '';
 
 
 //Create basic + - / * functions and a main calculator function
@@ -34,15 +36,36 @@ function multiplication(a, b){
    return a * b;
     }
 
-//create an operate function that uses the other functions and calls on the function and numbers
-//create a basic calculator with html and css with an equals button and a clear button (create a display with for the buttons)
 
-function operate(func){
-    func();
+//create an operate function that uses the other functions and calls on the function and numbers
+
+function operate(num1, num2, operator){
+    
+    if (operator == addition){
+
+        return addition(num1, num2);
+        
+    }
+    else if (operator == subtraction){
+
+        return subtraction(num1, num2);
+    }
+    else if (operator == division){
+
+        return division(num1, num2);
+    }
+    else if (operator == multiplication){
+
+        return multiplication(num1, num2);
+    }
+    else {
+        return "Please use an operator";
+    }
 
 }
 
 //create the functions that populate the display (should be storing the display value in a variable outside of the scope for use later)
+//TODO use the input from buttons to start the operate function and then display answer on the calculator
 
 function displayInput(digit){
   
